dbaa48c524074ff463b6ea5fd5325833db76bb1b
giftin.id <calistanafisaa@gmail.com>
2026-06-26 00:39:13 +0700

    Update style.js

--- asset/js/style.js
+++ asset/js/style.js
@@ -3,12 +3,12 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('I Love You, Athan').split('');
+        const titles = ('So,').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
  function play() {
-          //Link Audio Bisa Diganti
+          
           var audio = new Audio('anchor.mp3');
           audio.play();
         }
@@ -17,16 +17,15 @@
           if (index < titles.length) {
             titleElement.innerHTML += titles[index];
             index++;
-            setTimeout(appendTitle, 300); // 300ms delay per huruf
+            setTimeout(appendTitle, 300); 
           } else {
-            // Setelah teks utama selesai, tambahkan subtitle kecil
+           
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Thank you for loving me with all your lovee – ilysb bb!<33";
-            subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
-            subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
-            subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
+            subtitle.innerText = "can I be yours in the quiet moments and the loud ones too?";
+            subtitle.style.fontSize = '20px'; 
+            subtitle.style.marginTop = '2px'; 
+            subtitle.style.color = '#fff'; 
 
-            // Menambahkan subtitle di bawah #title
             titleElement.appendChild(subtitle);
           }
         }
